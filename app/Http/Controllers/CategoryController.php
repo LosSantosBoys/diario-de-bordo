a30a69918b5a08098821374cf62d3aa249882c91
Samuel Marques <samuel.s.marques@protonmail.com>
2022-10-10 20:13:12 -0300

    feat: adiciona controlador de categoria

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller {
+    public function index() {
+        $categories = Category::all();
+        return response()->json($categories);
+    }
+
+    public function show($titulo) {
+        $category = Category::where('titulo', $titulo)->first();
+        return response()->json($category);
+    }
+
+    public function create(Request $request) {
+        $rules = [
+            'titulo' => 'required|min:3|max:40'
+        ];
+
+        $feedback = [
+            'required' => 'O campo :attribute deve ser preenchido.',
+            'titulo.min' => 'O campo titulo deve ter no mínimo 3 caracteres.',
+            'titulo.max' => 'O campo titulo deve ter no máximo 40 caracteres.',
+        ];
+
+        $this->validate($request, $rules, $feedback);
+        
+        $category = new Category();
+        $category->titulo = $request->titulo;
+        
+        $category->save();
+        return response()->json($category);
+    }
+
+    public function update(Request $request, $titulo) {
+        $rules = [
+            'titulo' => 'required|min:3|max:40'
+        ];
+
+        $feedback = [
+            'required' => 'O campo :attribute deve ser preenchido.',
+            'titulo.min' => 'O campo titulo deve ter no mínimo 3 caracteres.',
+            'titulo.max' => 'O campo titulo deve ter no máximo 40 caracteres.',
+        ];
+
+        $this->validate($request, $rules, $feedback);
+        
+        $category = Category::where('titulo', $titulo)->first();
+        $titulo = $request->titulo;
+        
+        $category->save();
+        return response()->json($category);
+    }
+
+    public function delete($titulo) {
+        $category = Category::where('titulo', $titulo)->first();
+        $category->delete();
+
+        return response()->json('Categoria deletada com sucesso.');
+    }
+}
+
+?>

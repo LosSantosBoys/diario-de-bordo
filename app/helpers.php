dfdc8555768c3b1bb7195a360ddfbcd78ad22322
Samuel Marques <samuel.s.marques@protonmail.com>
2022-10-11 00:54:49 -0300

    feat: adiciona função que remove caracteres especiais

--- app/helpers.php
+++ app/helpers.php
@@ -106,4 +106,11 @@ if (!function_exists('remove_accents')) {
     }
 }
 
+if (!function_exists('remove_spc_chr')) {
+    $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+    $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+     
+    return preg_replace('/-+/', '-', $string); // Replaces multiple hyphens with single one.
+}
+
 ?>

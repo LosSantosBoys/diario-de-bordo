542a17543b33a087779f5a083badf76416059c57
Samuel Marques <samuel.s.marques@protonmail.com>
2022-10-12 15:24:15 -0300

    feat: adiciona slug

--- database/migrations/2022_10_10_152850_create_categories_table.php
+++ database/migrations/2022_10_10_152850_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('slug')->unique();
             $table->string('titulo')->unique();
             $table->timestamps();
         });

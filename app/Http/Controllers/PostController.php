10c996941df74ad6b46b0f05aa3ffb14af6a153c
Samuel Marques <samuel.s.marques@protonmail.com>
2022-10-17 11:08:09 -0300

    feat: adiciona try catch\

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,144 +12,192 @@ require_once __DIR__ . '/../../helpers.php';
 
 class PostController extends Controller
 {
-    public function index(Request $request) {
-        $posts = Post::query();
+    public function index(Request $request)
+    {
+        try {
+            $posts = Post::query();
 
-        if ($request->query('titulo')) {
-            $posts->where('titulo', 'LIKE', '%' . $request->query('titulo') . '%');
+            if ($request->query('titulo')) {
+                $posts->where('titulo', 'LIKE', '%' . $request->query('titulo') . '%');
+            }
+
+            if ($request->query('slug')) {
+                $posts->where('slug', 'LIKE', '%' . $request->query('slug') . '%');
+            }
+
+            $posts->where('dataDePublicacao', '<', now())
+                ->where('visivel', '=', 1)
+                ->orderBy('dataDePublicacao', 'ASC');
+
+            return new PostCollection($posts->get()->paginate(15));
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $th->getMessage()
+            ], 500);
         }
-
-        if ($request->query('slug')) {
-            $posts->where('slug', 'LIKE', '%' . $request->query('slug') . '%');
-        }
-
-        $posts->where('dataDePublicacao', '<', now())
-            ->where('visivel', '=', 1)
-            ->orderBy('dataDePublicacao', 'ASC');
-
-        return new PostCollection($posts->get()->paginate(15));
     }
 
-    public function show($slug) {
-        $post = Post::where('slug', $slug)->first();
-    
-        if ($post == null) {
+    public function show($slug)
+    {
+        try {
+            $post = Post::where('slug', $slug)->first();
+
+            if ($post == null) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Post não encontrado.'
+                ], 400);
+            }
+
+            return new PostResource($post);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function create(Request $request)
+    {
+        try {
+            $rules = [
+                'titulo' => 'required|min:3|max:70|unique:posts',
+                'conteudo' => 'required|min: 5',
+                'visivel' => 'required',
+                'dataDePublicacao' => 'required|date',
+            ];
+
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchido.',
+                'titulo.unique' => 'Um post com esse título já existe.',
+                'titulo.min' => 'O campo titulo deve ter no mínimo 3 caracteres.',
+                'titulo.max' => 'O campo titulo deve ter no máximo 70 caracteres.',
+                'conteudo.min' => 'O campo conteudo deve ter no mínimo 5 caracteres.',
+                'visivel.boolean' => 'O campo visivel precisa ser booleano `true` ou `false`.',
+                'dataDePublicacao.date' => 'O campo dataDePublicacao precisa ser uma data.',
+            ];
+
+            $this->validate($request, $rules, $feedback);
+
+            $slug = clean($request->titulo);
+            $checkPost = Post::where('slug', $slug)->first();
+
+            if ($checkPost != null) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Post já existe.'
+                ], 400);
+            }
+
+            $post = new Post();
+
+            $post->slug = $slug;
+            $post->titulo = trim($request->titulo);
+            $post->conteudo = trim($request->conteudo);
+            $post->categoriaId = $request->categoriaId;
+            $post->visivel = $request->visivel;
+            $post->dataDePublicacao = $request->dataDePublicacao;
+
+            $post->save();
+            return new PostResource($post);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function update(Request $request, $slug)
+    {
+        try {
+            $rules = [
+                'slug' => 'required|min:3',
+                'titulo' => 'required|min:3|max:70',
+                'conteudo' => 'required|min: 5',
+                'visivel' => 'required',
+                'dataDePublicacao' => 'required|date',
+            ];
+
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchido.',
+                'slug.min' => 'O campo slug deve ter no mínimo 3 caracteres.',
+                'titulo.min' => 'O campo titulo deve ter no mínimo 3 caracteres.',
+                'titulo.max' => 'O campo titulo deve ter no máximo 70 caracteres.',
+                'conteudo.min' => 'O campo conteudo deve ter no mínimo 5 caracteres.',
+                'visivel.boolean' => 'O campo visivel precisa ser booleano `true` ou `false`.',
+                'dataDePublicacao.date' => 'O campo dataDePublicacao precisa ser uma data.',
+            ];
+
+            $this->validate($request, $rules, $feedback);
+
+            $post = Post::where('slug', $slug)->first();
+
+            if (!$post) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Post não encontrado.'
+                ], 400);
+            }
+
+            $newSlug = clean($request->slug);
+            $post->slug = $newSlug;
+            $post->titulo = trim($request->titulo);
+            $post->conteudo = trim($request->conteudo);
+            $post->categoriaId = $request->categoriaId;
+            $post->visivel = $request->visivel;
+            $post->dataDePublicacao = $request->dataDePublicacao;
+
+            $post->save();
+            return new PostResource($post);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function delete($slug)
+    {
+        try {
+            $post = Post::where('slug', $slug)->first();
+
+            if ($post) {
+                $post->delete();
+                return response()->json(null, 204);
+            }
+
             return response()->json([
                 'status' => 'error',
                 'message' => 'Post não encontrado.'
             ], 400);
-        }
-
-        return new PostResource($post);
-    }
-
-    public function create(Request $request) {
-        $rules = [
-            'titulo' => 'required|min:3|max:70|unique:posts',
-            'conteudo' => 'required|min: 5',
-            'visivel' => 'required',
-            'dataDePublicacao' => 'required|date',
-        ];
-
-        $feedback = [
-            'required' => 'O campo :attribute deve ser preenchido.',
-            'titulo.unique' => 'Um post com esse título já existe.',
-            'titulo.min' => 'O campo titulo deve ter no mínimo 3 caracteres.',
-            'titulo.max' => 'O campo titulo deve ter no máximo 70 caracteres.',
-            'conteudo.min' => 'O campo conteudo deve ter no mínimo 5 caracteres.',
-            'visivel.boolean' => 'O campo visivel precisa ser booleano `true` ou `false`.',
-            'dataDePublicacao.date' => 'O campo dataDePublicacao precisa ser uma data.',
-        ];
-
-        $this->validate($request, $rules, $feedback);
-
-        $slug = clean($request->titulo);
-        $checkPost = Post::where('slug', $slug)->first();
-    
-        if ($checkPost != null) {
+        } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Post já existe.'
-            ], 400);
+                'message' => $th->getMessage()
+            ], 500);
         }
-
-        $post = new Post();
-
-        $post->slug = $slug;
-        $post->titulo = trim($request->titulo);
-        $post->conteudo = trim($request->conteudo);
-        $post->categoriaId = $request->categoriaId;
-        $post->visivel = $request->visivel;
-        $post->dataDePublicacao = $request->dataDePublicacao;
-        
-        $post->save();
-        return new PostResource($post);
     }
 
-    public function update(Request $request, $slug) {
-        $rules = [
-            'slug' => 'required|min:3',
-            'titulo' => 'required|min:3|max:70',
-            'conteudo' => 'required|min: 5',
-            'visivel' => 'required',
-            'dataDePublicacao' => 'required|date',
-        ];
+    public function search(Request $request)
+    {
+        try {
+            $query = $request->query('q');
 
-        $feedback = [
-            'required' => 'O campo :attribute deve ser preenchido.',
-            'slug.min' => 'O campo slug deve ter no mínimo 3 caracteres.',
-            'titulo.min' => 'O campo titulo deve ter no mínimo 3 caracteres.',
-            'titulo.max' => 'O campo titulo deve ter no máximo 70 caracteres.',
-            'conteudo.min' => 'O campo conteudo deve ter no mínimo 5 caracteres.',
-            'visivel.boolean' => 'O campo visivel precisa ser booleano `true` ou `false`.',
-            'dataDePublicacao.date' => 'O campo dataDePublicacao precisa ser uma data.',
-        ];
+            $posts = DB::table('posts')
+                ->where('titulo', 'LIKE', "%{$query}%")
+                ->orWhere('slug', 'LIKE', "%{$query}%")
+                ->get();
 
-        $this->validate($request, $rules, $feedback);
-
-        $post = Post::where('slug', $slug)->first();
-    
-        if (!$post) {
+            return new PostCollection($posts);
+        } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Post não encontrado.'
-            ], 400);
+                'message' => $th->getMessage()
+            ], 500);
         }
-
-        $newSlug = clean($request->slug);
-        $post->slug = $newSlug;
-        $post->titulo = trim($request->titulo);
-        $post->conteudo = trim($request->conteudo);
-        $post->categoriaId = $request->categoriaId;
-        $post->visivel = $request->visivel;
-        $post->dataDePublicacao = $request->dataDePublicacao;
-
-        $post->save();
-        return new PostResource($post);
-    }
-
-    public function delete($slug) {
-        $post = Post::where('slug', $slug)->first();
-        
-        if ($post) {
-            $post->delete();
-            return response()->json(null, 204);
-        }
-
-        return response()->json([
-            'status' => 'error',
-            'message' => 'Post não encontrado.'
-        ], 400);
-    }
-
-    public function search(Request $request) {
-        $query = $request->query('q');
-
-        $posts = DB::table('posts')
-            ->where('titulo', 'LIKE', "%{$query}%")
-            ->orWhere('slug', 'LIKE', "%{$query}%")
-            ->get();
-
-        return new PostCollection($posts);
     }
 }

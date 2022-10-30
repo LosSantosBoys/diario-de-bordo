d7377e03500c17d8ff49f97f24932110dbac1388
Samuel Marques <samuel.s.marques@protonmail.com>
2022-10-30 16:01:21 -0300

    feat: adiciona post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+<main class="container col-8">
+    @if(isset($posts))
+        @foreach ($posts as $post)
+        <div class="row mb-5">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 d-flex">
+                <div class="d-none d-sm-block">
+                    <img src="" width="200" height="200" alt="" />
+                </div>
+                <div class="post-card">
+                    <div class="post-title">
+                        <h3>{{ new \Moment\Moment($post->dataDePublicacao).format() }} - {{ $post->titulo }}</h3>
+                    </div>
+                    <div class="post-content">
+                        {{ $post->conteudo }}
+                    </div>
+                    <div class="post-more">
+                        <a
+                            class="text-decoration-none text-lg text-lg-hover"
+                            href="./pages/post.html">
+                                Leia mais
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+        @endforeach
+    @endif
+</main>
 @endsection

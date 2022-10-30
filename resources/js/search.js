3d29342e4e7eabc1d2979cc5e0c8df851df01f45
Samuel Marques <samuel.s.marques@protonmail.com>
2022-10-30 20:52:43 -0300

    fix: corrige rota de pesquisa

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,12 +1,12 @@
 const form = document.getElementById("search-form");
 const query = document.getElementById("search-query");
-const search = "posts/search=";
+const search = "/posts/search?q=";
 
 function submit(event) {
   event.preventDefault();
 
   const url = search + query.value;
-  window.open(url, "_blank").focus();
+  window.location.href = url;
 }
 
 form.addEventListener("submit", submit);

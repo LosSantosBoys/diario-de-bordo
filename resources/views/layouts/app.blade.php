ebe26af3f8aabbfa74fb0afb300e28439a107fdc
Samuel Marques <samuel.s.marques@protonmail.com>
2022-10-30 21:07:01 -0300

    feat: adiciona foreach de categorias

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,6 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    
 </head>
 <body>
         <!-- NAVBAR -->
@@ -34,12 +33,11 @@
 
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav nav-links">
-                    <a class="nav-item nav-link" href="./index.html"
-                        >Categoria 1</a
-                    >
-                    <a class="nav-item nav-link" href="#">Categoria 2</a>
-                    <a class="nav-item nav-link" href="#">Categoria 3</a>
-                    <a class="nav-item nav-link" href="#">Categoria 4</a>
+                    @foreach(\App\Models\Category::all() as $category)
+                        <a class="nav-item nav-link" href="/categories/{{ $category->slug }}">
+                            {{ $category->titulo }}
+                        </a>
+                    @endforeach
                 </div>
             </div>
 
